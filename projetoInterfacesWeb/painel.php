ebc217b9b1b71f4d8318f034458bb099c9b1dc0d
luan004 <38366327+luan004@users.noreply.github.com>
2023-03-23 20:06:47 -0300

    Add files via upload

---
+++ projetoInterfacesWeb/painel.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Luan - Projeto de Interfaces WEB</title>
+    <link rel="stylesheet" type="text/css" href="stylePainel.css" />
+</head>
+<body>
+    <div class="header">
+        <img class="adminAvatar"  src="avatar.jpg">
+        <label><?=$_GET["email"]?></label>
+    </div>
+
+    <div class="mensagem">Bem Vindo</div>
+</body>
+</html>
